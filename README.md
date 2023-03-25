d862443efcc8d10466279b967c488eb1bff9f124
salvo720 <salvocelano91@gmail.com>
2023-03-25 20:09:23 +0100

    feat(loSapevi) : aggiornato file index.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # linkedin
+
+questo repo fa rimenti ai post di linkedin ha 2 settocartelle principlali loSapevi e Coding 
---
+++ loSapevi/index.js
@@ -0,0 +1,11 @@
+/*
+    LoSapevi e una raccolta di trick e shortcut dui cui i non ero a conoscenza e se sei qui nemmeno tu :) :
+    1) json formatter 
+    
+    
+    
+    in futuro : 
+    1) node video 3 "node esegue codice non bloccante "
+    2) dailydev
+
+*/
