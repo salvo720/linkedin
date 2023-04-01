6c4d052e350ade772ef4a887428e927548a222a4
salvo720 <salvocelano91@gmail.com>
2023-04-01 19:09:25 +0200

    feat(esplorazione ) : aggiunta cartella esplorazione>angular> index.js

---
+++ Esplorazione/Angular/index.js
@@ -0,0 +1,12 @@
+/*
+    esplorazione angular e una raccolta di quello che imparo su angular :
+    1) iniziene diepndenze tramite  inject() vs costruttore 
+    
+    
+    
+
+
+
+    possibili : pipe async 
+
+*/
