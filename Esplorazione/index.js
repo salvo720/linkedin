45b9270c4b8cc29dfb7dd66d08f522b0c07f3b63
salvo720 <salvocelano91@gmail.com>
2023-04-08 20:36:12 +0200

    feat(esplorazione) : aggiornato file index.js con i riferimenti a nuovo post

---
+++ Esplorazione/index.js
@@ -0,0 +1,16 @@
+/*
+    esplorazione angular e una raccolta di quello che imparo su  :
+    1) angular : iniziene diepndenze tramite  inject() vs costruttore 
+    2) Spring Boot JPA : connessione al database con Spring Boot e JPA nuovo metodo vs vecchio metodo
+
+
+
+    
+    
+    
+
+
+
+    possibili : pipe async , keyframe css vs animazioni angular animation [ transition ] , java connessione db vecchio vs nuovo 
+
+*/
