d2fa26948ec4698568917613267475fb8900a013
salvo720 <salvocelano91@gmail.com>
2023-05-06 20:23:36 +0200

    feat(esplorazione) : aggiornato con i nuovi post

--- Esplorazione/index.js
+++ Esplorazione/index.js
@@ -2,6 +2,9 @@
     esplorazione angular e una raccolta di quello che imparo su  :
     1) angular : iniziene diepndenze tramite  inject() vs costruttore 
     2) Spring Boot JPA : connessione al database con Spring Boot e JPA nuovo metodo vs vecchio metodo
+    3) esplorando #3 git: branch e pull request parte 1
+    4) esplorando #4 git: branch e pull request parte 2
+    5) regex : analisi e dettagli sulla regex del meme , what scare programmers ??
 
 
 
@@ -11,6 +14,6 @@
 
 
 
-    possibili : pipe async , keyframe css vs animazioni angular animation [ transition ] , java connessione db vecchio vs nuovo 
+    possibili : pipe async , keyframe css vs animazioni angular animation [ transition ] 
 
 */
